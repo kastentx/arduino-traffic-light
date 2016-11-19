4bbe920534223348867436a27ef58a454404e81b
Nick Kasten <kastencode@gmail.com>
2016-11-19 02:13:21 -0600

    formatted code

--- my-traffic-light.ino
+++ my-traffic-light.ino
@@ -1,45 +1,43 @@
+// Arduino Traffic Light
+// Author: Nick Kasten
+
+/* Global Variables */
+// specifies ports we will have LEDs connected to
 int red     = 13;
 int yellow  = 12;
 int green   = 11;
 
-
-
+/* Setup Function */
 void setup() {
-  // put your setup code here, to run once:
+  // sets pins to OUTPUT mode (so we can turn on LEDS)
   pinMode(red, OUTPUT);
   pinMode(yellow, OUTPUT);
   pinMode(green, OUTPUT);
 }
-
+/* Main Loop */
 void loop() {
-  // put your main code here, to run repeatedly:
+  // this loop runs repeatedly
   changeLights();
 }
+/* Utility Functions */
  void changeLights() {
-
   //green light go!
   digitalWrite(green, HIGH);
-  delay(2000);
-  
+  delay(2000); 
   // green and yellow for 1.5 seconds
   digitalWrite(yellow, HIGH);
   delay(1500);
-  
   // green off, yellow for 2 seconds
   digitalWrite(green, LOW);
   delay(2000);
-
   // turn off yellow, red on for 3 seconds
   digitalWrite(yellow, LOW);
   digitalWrite(red, HIGH);
   delay(3000);
-
   // turn off red, turn on green
   digitalWrite(red, LOW);
   digitalWrite(green, HIGH);  
   delay(2000);
-
   // all lights off
   digitalWrite(green, LOW);
  }
-
